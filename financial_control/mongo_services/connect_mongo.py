b81decca86272131be26b0f0e1164cab171d935f
BrunoASNascimento <bruno-asn@outlook.com>
2022-10-29 23:29:27 -0300

    Conection with mongo done

--- financial_control/mongo_services/connect_mongo.py
+++ financial_control/mongo_services/connect_mongo.py
@@ -2,18 +2,44 @@ from pymongo import MongoClient, database
 import os
 
 
-def get_database() -> database.Database:
-    MONGO_USER = os.environ.get("MONGO_USER")
-    MONGO_PASS = os.environ.get("MONGO_PASS")
+class MongoServices(object):
 
-    # Provide the mongodb atlas url to connect python to mongodb using pymongo
-    CONNECTION_STRING = f"mongodb+srv://{MONGO_USER}:{MONGO_PASS}@cluster-study.xtphu.mongodb.net/admin?readPreference=primary&authSource=admin&authMechanism=SCRAM-SHA-1"
+    DATABASE = None
 
-    # Create a connection using MongoClient. You can import MongoClient or use pymongo.MongoClient
-    client = MongoClient(CONNECTION_STRING)
+    # TODO: Write the function insertMany
+    # TODO: Write the function removeMany
 
-    # Create the database for our example (we will use the same database throughout the tutorial
-    return client['financial-control']
+    @staticmethod
+    def initialize() -> database.Database:
+        MONGO_USER = os.environ.get("MONGO_USER")
+        MONGO_PASS = os.environ.get("MONGO_PASS")
+
+        # Provide the mongodb atlas url to connect python to mongodb using pymongo
+        CONNECTION_URL = f"mongodb+srv://{MONGO_USER}:{MONGO_PASS}@cluster-study.xtphu.mongodb.net/admin?readPreference=primary&authSource=admin&authMechanism=SCRAM-SHA-1"
+
+        data_base_name = 'financial-control'
+        # Create a connection using MongoClient. You can import MongoClient or use pymongo.MongoClient
+        client = MongoClient(CONNECTION_URL)
+
+        # Create the database for our example (we will use the same database throughout the tutorial
+        MongoServices.DATABASE = client[data_base_name]
+
+    @staticmethod
+    def insert(collection, data):
+        id_cod = MongoServices.DATABASE[collection].insert(data)
+        return id_cod
+
+    @staticmethod
+    def find(collection, query):
+        return MongoServices.DATABASE[collection].find(query)
+
+    @staticmethod
+    def find_one(collection, query):
+        return MongoServices.DATABASE[collection].find_one(query)
+
+    @staticmethod
+    def delete(collection, query):
+        MongoServices.DATABASE[collection].remove(query)
 
 
 # # This is added so that many files can reuse the function get_database()
@@ -21,9 +47,15 @@ def get_database() -> database.Database:
 #     from dotenv import find_dotenv, load_dotenv
 #     load_dotenv(find_dotenv())
 #     # Get the database
-#     dbname = get_database()
-#     print(type(dbname))
-#     item_details = dbname['accounts'].find()
+#     MongoServices.initialize()
+
+#     item_details = MongoServices.find("accounts", {})
 #     for item in item_details:
 #         # This does not give a very readable output
 #         print(item)
+
+#     data = {
+#         "testStr": "test",
+#         "testNumber": 1234
+#     }
+#     print(MongoServices.insert("test", data))
--- tests/unit_test.py
+++ tests/unit_test.py
@@ -1,25 +1,42 @@
-"""Unit Test - Package - index"""
-
-from financial_control.package import example
-from financial_control.mongo_services.connect_mongo import get_database
+# from financial_control.package import example
+from financial_control.mongo_services.connect_mongo import MongoServices
 import pytest
 from pytest import mark
 from dotenv import find_dotenv, load_dotenv
 
+from pymongo import database, cursor
+
 
 class TestExample:
-
-    def test_example(self):
-        """Method to test Example"""
-        testexample = example()
-
-        # Then
-        assert testexample == 10
 
     @mark.test_connection_mongo
     def test_mongo_connection(self):
         load_dotenv(find_dotenv())
-        dbname = get_database()
+        MongoServices.initialize()
+        dbname = MongoServices.DATABASE
+        assert type(dbname) == database.Database
+
+    @mark.test_read_document
+    def test_read_document(self):
+        data_read = MongoServices.find("accounts", {})
+        assert type(data_read) == cursor.Cursor
+
+    @mark.test_insert_read_delete
+    def test_insert_read_delete(self):
+        data = {
+            "testStr": "test",
+            "testNumber": 1234
+        }
+        id_cod = MongoServices.insert("test", data)
+        data_read = MongoServices.find_one("test", id_cod)
+
+        assert data == data_read
+
+        MongoServices.delete("test", {"_id": id_cod})
+
+        data_read = MongoServices.find_one("test", id_cod)
+
+        assert data_read == None
 
 
 # To execute tests wihr mark
